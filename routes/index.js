345ead76a99615a81d09205f14c89d81bb2fb131
Karthik <Karthikmuthyala20@gmail.com>
2018-11-21 17:06:44 -0600

    d

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+/**
+ * @index.js - manages all routing
+ *
+ * router.get when assigning to a single request
+ * router.use when deferring to a controller
+ *
+ * @requires express
+ */
+
+const express = require('express')
+const LOG = require('../utils/logger.js')
+
+LOG.debug('START routing')
+const router = express.Router()
+
+// Manage top-level request first
+router.get('/', (req, res, next) => {
+  //LOG.debug('Request to /')
+  res.render('index.ejs', { title: 'Express App' })
+})
+
+// Defer path requests to a particular controller
+// router.use('/about', require('../controllers/about.js'))
+ router.use('/order', require('../controllers/order.js'))
+ router.use('/customer', require('../controllers/customer.js'))
+ router.use('/product', require('../controllers/product.js'))
+ 
+LOG.debug('END routing')
+module.exports = router
